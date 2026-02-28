dcedae1701c2116c6f825d358d171c1fa4f4a252
Kaustubh Sawant <kaustubhsawant111@gmail.com>
2026-02-28 23:13:23 +0530

    feat: added interface to define the service behaviour

---
+++ TodoApi/Interfaces/ITodoService.cs
@@ -0,0 +1,19 @@
+﻿using TodoApi.DTOs.RequestDTOs;
+using TodoApi.DTOs.ResponseDTOs;
+
+namespace TodoApi.Interfaces
+{
+    public interface ITodoService
+    {
+        Task<Todo> CreateTodoAsync(CreateTodo createTodo);
+        
+        Task<Todo?> GetTodoByIdAsync(int id);
+
+        Task<IEnumerable<Todo>> GetAllTodosAsync();
+
+        Task<Todo> UpdateTodoAsync(int id, UpdateTodo updateTodo);
+
+        Task<bool> DeleteTodoAsync(int id);
+
+    }
+}
